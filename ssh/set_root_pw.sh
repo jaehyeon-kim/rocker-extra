6b902dd89d6fa6837fd3e5c806f14350d34a0481
Jaehyeon Kim <jaehyeon.kim@zoho.com>
2018-04-03 09:19:35 +1000

    ssh key read directly by volume mapping

--- ssh/set_root_pw.sh
+++ ssh/set_root_pw.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
-if [ -z "${SSH_KEY}" ]; then
-	echo "=> Please pass your public key in the SSH_KEY environment variable"
-	exit 1
-fi
+#if [ -z "${SSH_KEY}" ]; then
+#	echo "=> Please pass your public key in the SSH_KEY environment variable"
+#	exit 1
+#fi
 
 for MYHOME in /home/rstudio; do
 	echo "=> Adding SSH key to ${MYHOME}"
 	mkdir -p ${MYHOME}/.ssh
 	chmod go-rwx ${MYHOME}/.ssh
-	echo "${SSH_KEY}" > ${MYHOME}/.ssh/authorized_keys
+	cat ${MYHOME}/pub/id_rsa.pub > ${MYHOME}/.ssh/authorized_keys
+	#echo "${SSH_KEY}" > ${MYHOME}/.ssh/authorized_keys
 	chmod go-rw ${MYHOME}/.ssh/authorized_keys
 	echo "=> Done!"
 done
